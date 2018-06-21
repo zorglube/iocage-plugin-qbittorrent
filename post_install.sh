14694275d01ebdb90e0f6f51f14f242511884e9b
Martin Wilke <miwi@FreeBSD.org>
2018-06-21 21:36:49 +0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-# Enable the service
-sysrc -f /etc/rc.conf qbittorrent_enable="YES"
-
-# Start the service
-service qbittorrent start 2>/dev/null
+#Install flavor port
+pkg install qbittorrent-nox
