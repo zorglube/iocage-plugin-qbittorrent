f29e885526ea602e287a5f663464ecbe48151c48
Martin Wilke <miwi@FreeBSD.org>
2018-07-17 14:26:48 +0800

    - Echo

--- post_install.sh
+++ post_install.sh
@@ -8,9 +8,6 @@ rm /usr/local/share/doc/qbittorrent/AUTHORS
 pkg install qbittorrent-nox
 
 #auto start after jail reboot
-/usr/local/bin/qbittorrent-nox -d > /etc/rc.local
-
-#start after first install
-/usr/local/bin/qbittorrent-nox -d
+echo /usr/local/bin/qbittorrent-nox -d > /etc/rc.local
 
 echo "Please open the URL and Login with Username: admin, Password: adminadmin"
