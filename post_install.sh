9232feb3e020a909cddb3562b72a174212fcac8a
Martin Wilke <miwi@FreeBSD.org>
2018-07-17 13:58:46 +0800

    - First Start - Start after reboot

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
+
+# Remove a conflict file
+rm /usr/local/share/doc/qbittorrent/AUTHORS
+
 #Install flavor port
 pkg install qbittorrent-nox
+
+#auto start after jail reboot
+/usr/local/bin/qbittorrent-nox -d > /etc/rc.local
+
+#start after first install
+/usr/local/bin/qbittorrent-nox -d
+
+echo "Please open the URL and Login with Username: admin, Password: adminadmin"
